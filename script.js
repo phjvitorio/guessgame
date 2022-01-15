021d159a6f64d3a47308ba4bb531f8857d4eb357
phjvitorio <phjvitorio@gmail.com>
2022-01-14 21:04:41 -0300

    Final changes

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const answer = getRandomInt(1, 20);
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = document.querySelector('.guess').value;
+
+  if (guess > answer) {
+    document.querySelector('.message').textContent = 'Too high!';
+    score -= 1;
+    document.querySelector('.score').innerHTML = score;
+  }
+
+  if (guess < answer) {
+    document.querySelector('.message').textContent = 'Too low!';
+    score -= 1;
+    document.querySelector('.score').innerHTML = score;
+  }
+
+  if (guess == answer) {
+    document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('.highscore').innerHTML = score;
+    document.querySelector('.number').textContent = answer;
+
+    document.querySelector('body').style.backgroundColor = 'green';
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  console.log('clicked');
+  window.location.reload();
+});
